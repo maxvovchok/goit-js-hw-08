cc5839ae7ca143f8b3df5d922f3906bde287036c
maxvovchok <vovchokmakcim@gmail.com>
2024-02-08 12:06:04 +0200

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -4,7 +4,6 @@ import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
 
 console.log(galleryItems);
 
